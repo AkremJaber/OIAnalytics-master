2385c5cdc7ee894d3efbf46525b12f36ad2ac125
Akrem Jaber <akrem.jaber@esprit.tn>
2022-09-16 10:08:33 +0100

    dictionary

--- src/app/create-popup/create-popup.component.ts
+++ src/app/create-popup/create-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dictionary, forEach } from 'lodash';
 import { AaduserService } from '../Shared/Services/AADUserService/aaduser.service';
 import { TenantService } from '../Shared/Services/TenantService/tenant.service';
 
@@ -32,15 +33,34 @@ export class CreatePopupComponent implements OnInit {
   this.selectedValueAADUser = this.ADuserService.getDropDownTextAADUser(this.mySelectAADUser,this.t);
   
    }
-   CreateTenantListAADUser(name:any,list:any){
+  //  CreateTenantListAADUser(name:any,list:any){
+  //   name=this.e
+  //   list=this.selectedValueAADUser
+  //   // console.log(name)
+  //   // console.log(list)
+  //   this.ADuserService.CreateListAADUser(name,list).subscribe()
+  //   window.location.reload();
+  //  }
+
+   CreateTenantDictAADUser(name:any,list:any){
     name=this.e
     list=this.selectedValueAADUser
-    // console.log(name)
-    // console.log(list)
-    this.ADuserService.CreateListAADUser(name,list).subscribe()
-    window.location.reload();
+    let map = new Map<string,string>();
+    let dictionary:any;
+    for (let item of list) {
+      let test = new Object();
+      test={ uid:item.uiD_Person , access:'Admin'};
+      let data: any
+      data.set(test);
+      map.set( item.uiD_Person , 'Admin'); 
+      dictionary = Object.assign({}, ...data.map((x:any) => ({[x.uid]: x.access})));
+      console.log(dictionary.uid);
+      console.log(map.values())
+  }
+  
+    this.ADuserService.CreateListAADUser(name,dictionary).subscribe()
+    // window.location.reload();
    }
-
 
   ngOnInit(): void {
     this.getAADUser();
